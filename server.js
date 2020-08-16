d36850e40f7734b378ad6e870b71b2193fcdc535
ganesh <ganesh.dc@gmail.com>
2020-08-16 17:52:44 +0530

    added yenv for better organising of envirornment variables and added logic for jwt verfication

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
-
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const yenv = require('yenv')
+const env = yenv('./env.yaml', {env: 'development'})
+
 const app = express()
+
 
 app.listen(3000)
 app.use(express.json())
@@ -19,7 +21,7 @@ const posts = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('hello, welcome to Nodejs jwt app.')
 })
 
 
@@ -27,10 +29,23 @@ app.post('/login', (req,res) => {
   //Authenticate User
   const username = req.body.username
   const user = { username: username }
-  const accessToken = jwt.sign(user, process.env.AUTH_TOKEN_SECRET)
+  const accessToken = jwt.sign(user, env.AUTH_TOKEN_SECRET)
   res.json({accessToken: accessToken})
 })
 
-app.get('/posts', (req, res) => {
+app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts)
 })
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, env.AUTH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+    next()
+  })
+}
